54d671ca401b6068873c10af3db33a0c7e57918d
Nachiappan <nachiapan@gmail.com>
2020-04-09 22:03:51 +0530

    Added more comments

--- dist/index.js
+++ dist/index.js
@@ -532,10 +532,12 @@ async function prepare(){
 		var branch = github.context.ref.replace("refs\/heads\/", "");
 		setResult(eventName, github.context.sha, branch);
 	} else if(eventName == "repository_dispath") {
+		console.log('action started inside dispatch');
 		var client_payload = github.context.payload.client_payload;
 		if(typeof(client_payload.buildBranch) === "undefined") throw new Error("While triggering with repository-dispatch, buildBranch property is not set");
 		if(typeof(client_payload.buildSha) === "undefined") throw new Error("While triggering with repository-dispatch,buildSha property is not set");
 		setResult(eventName, client_payload.buildSha, client_payload.buildBranch);
+		console.log('before git command');
 		await executeBashCommand(`git checkout -f ${client_payload.buildSha}`);
 	} else {
 		throw new Error(`The trigger is unknown: ${github.context.eventName}. The supported triggers are push and respository-dispatch`)
