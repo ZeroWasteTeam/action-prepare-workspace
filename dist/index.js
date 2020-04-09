97f757489fd8ee72f390a3b792854be4d08b43c5
Nachiappan <nachiapan@gmail.com>
2020-04-10 01:14:22 +0530

    done

--- dist/index.js
+++ dist/index.js
@@ -521,52 +521,37 @@ if(cpload != undefined) {
   buildSha = cpload.buildSha;  
 }
 
-console.log(eventName);
-console.log(sha);
-console.log(branch);
-console.log(buildSha);
-console.log(buildBranch);
+console.log(`Event Name: ${eventName}`);
+console.log(`Current Commit: ${sha}`);
+console.log(`Current Branch: ${branch}`);
+console.log(`BuildSha : ${buildSha}`);
+console.log(`BuildBranch: ${buildBranch}`);
 
+function setResult(eventName, sha, branch) {
+	core.setOutput("build-sha", sha);
+	core.setOutput("build-branch", branch);
+}
 
-/*
 async function executeBashCommand(command) {
   const res = await exec(command);
   const { stdout, stderr } = res;
   return stdout.replace(/\n/g,'').replace(/\r/g,'');
 }
 
-function setResult(eventName, sha, branch) {
-	core.setOutput("build-sha", sha);
-	core.setOutput("build-branch", branch);
-	console.log(`The build is due to a ${eventName}`);
-	console.log(`The build sha : ${sha}`);
-	console.log(`The build branch : ${branch}`);
-}
-
 async function prepare(){
-
-	
-	var eventName =  github.context.eventName;
-	console.log('start of prepare method'+eventName);
 	if(eventName == "push") {
-		var branch = github.context.ref.replace("refs\/heads\/", "");
 		setResult(eventName, github.context.sha, branch);
 	} else if(eventName == "repository_dispath") {
-		console.log('action started inside dispatch');
-		var client_payload = github.context.payload.client_payload;
-		if(typeof(client_payload.buildBranch) === "undefined") throw new Error("While triggering with repository-dispatch, buildBranch property is not set");
-		if(typeof(client_payload.buildSha) === "undefined") throw new Error("While triggering with repository-dispatch,buildSha property is not set");
-		setResult(eventName, client_payload.buildSha, client_payload.buildBranch);
-		console.log('before git command');
+		if(buildSha == "") throw new Error("While triggering with repository-dispatch, buildBranch property is not set");
+		if(buildBranch == "") throw new Error("While triggering with repository-dispatch,buildSha property is not set");
+		setResult(eventName, buildSha, buildBranch);
 		await executeBashCommand(`git checkout -f ${client_payload.buildSha}`);
 	} else {
 		throw new Error(`The trigger is unknown: ${github.context.eventName}. The supported triggers are push and respository-dispatch`)
 	}
-		
 }
 
 prepare().then(x => { }).catch(x => core.setFailed(x.message));
-*/
 
 /***/ }),
 
